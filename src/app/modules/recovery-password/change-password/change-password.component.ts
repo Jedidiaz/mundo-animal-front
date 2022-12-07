1f371633d71975997f285b0d8f349e02be6841c0
Jedidiaz <jedidiazfagundez@gmail.com>
2022-12-07 13:42:31 -0500

    Correcciones de validaciones en cambio de contraseña

--- src/app/modules/recovery-password/change-password/change-password.component.ts
+++ src/app/modules/recovery-password/change-password/change-password.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { UsersService } from '../../services/users/users.service';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
+  providers: [MessageService]
 })
 export class ChangePasswordComponent implements OnInit {
   payload: any;
 
-  constructor( private service: UsersService, private _route: ActivatedRoute ) { }
+  formChangePass!: FormGroup;
+  constructor( private service: UsersService, private _route: ActivatedRoute, public messageService: MessageService, fromBuilder: FormBuilder ) {
+    this.formChangePass = fromBuilder.group({
+      password_1: ['', Validators.required],
+      password_2: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
     this._route.queryParams.subscribe({
@@ -21,17 +30,35 @@ export class ChangePasswordComponent implements OnInit {
     })
   }
 
-  changePass(pass1: string, pass2: string){
-    if (pass1 == pass2){
-      let params = {payload: this.payload.payload, password: pass1}
+  changePass(){
+    if (this.formChangePass.value.password_1 == this.formChangePass.value.password_2){
+      let params = {payload: this.payload.payload, password: this.formChangePass.value.password_1}
       console.log(params)
       this.service.postChangePass(params).subscribe({
         next: (data)=> {
           console.log(data)
-        }, error: (err)=> {console.log(err)}
+        }, error: (err)=> {
+          console.log(params)
+          console.log(err.error.message)
+          if (err.error.message === 'Token expired, please try again'){
+            this.showErrorToken();
+          }
+        }
       })
     }else{
-      console.log('La contraseña no coincide')
+      this.showErrorPass();
     }
+  }
+
+  showErrorPass(){
+    this.messageService.add({severity:'error', summary: 'Error', detail: '¡La contraseña no coincide!'});
+  }
+
+  showErrorToken(){
+    this.messageService.add({severity:'error', summary: 'Error', detail: '¡El token expiró!'});
+  }
+
+  showSuccess(){
+    this.messageService.add({severity:'success', summary: 'Success', detail: '¡Se cambió la contraseña correctamente!'});
   }
 }
--- src/app/modules/recovery-password/recovery-password.component.ts
+++ src/app/modules/recovery-password/recovery-password.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { UsersService } from '../services/users/users.service';
 
 @Component({
   selector: 'app-recovery-password',
   templateUrl: './recovery-password.component.html',
-  styleUrls: ['./recovery-password.component.css']
+  styleUrls: ['./recovery-password.component.css'],
+  providers: [MessageService]
 })
 export class RecoveryPasswordComponent implements OnInit {
+  formForgotPass!: FormGroup;
 
-  constructor( private service: UsersService ) { }
+  constructor( private service: UsersService, public messageService: MessageService, formBuilder: FormBuilder ) {
+    this.formForgotPass = formBuilder.group({
+      email: ['', Validators.required]
+      })
+  }
 
   ngOnInit(): void {
   }
 
-  recovery(email: string){
-    let Email = {email: email}
+  recovery(){
+    let Email = {email: this.formForgotPass.value.email}
     this.service.recoveryPassword(Email).subscribe({
       next: (data) => {
-        console.log(data)
-      }, error: (err)=> {console.log(err)}
+        this.showSuccess();
+      }, error: (err)=> {
+        if(err.status === 404){
+          this.showError();
+        }
+      }
     })
+  }
+
+  showSuccess(){
+    this.messageService.add({severity:'success', summary: 'Success', detail: 'Se envio un email a: ' + this.formForgotPass.value.email});
+  }
+
+  showError(){
+    this.messageService.add({severity:'error', summary: 'Error', detail: 'Correo no registrado'});
   }
 
 }
